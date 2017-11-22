64a34c446ff83390585ae066f69a6a50b97d56e1
Kishan P Patel <kishanppatel19@gmail.com>
2017-11-22 18:39:34 +0530

    Update E-mail

--- identity/kishpatel1998.sh
+++ identity/kishpatel1998.sh
@@ -1,4 +1,4 @@
 git config --global user.name "kishpatel1998"
-git config --global user.email "kishannn.p.19@gmail.com"
+git config --global user.email "kishanppatel19@gmail.com"
 echo Welcome Kishan
 echo Work in darkness to serve the light !
